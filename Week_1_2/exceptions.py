b5e4d02bd820c9e5df99b47fded7f05a192b7e66
hooman-b <h.bahrdo@st.hanze.nl>
2023-06-02 23:26:20 +0330

    add the exceptions module

---
+++ Week_1_2/exceptions.py
@@ -0,0 +1,15 @@
+class NotIsotope(Exception):
+
+    """
+    type: exception class
+    explanation: this error raises when two atoms are not isotpes
+    output: an error raises NotIsotope
+    """
+    def __init__(self,proton_number1, proton_number2):
+        self.proton_number1 = proton_number1
+        self.proton_number2 = proton_number2
+        super().__init__()
+
+    def __str__(self) -> str:
+        return f"The atoms with proton numbers {self.proton_number1} and {self.proton_number2} are not isotopes."
+         
